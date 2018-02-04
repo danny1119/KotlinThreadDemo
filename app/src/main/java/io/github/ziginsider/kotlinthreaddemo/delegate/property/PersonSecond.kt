177fd129358d5bfd1170b6c8322b0080153e16cc
ziginsider <ziginsider@gmail.com>
2018-02-04 15:13:15 +0300

    delegate add PersonSecond class

---
+++ app/src/main/java/io/github/ziginsider/kotlinthreaddemo/delegate/property/PersonSecond.kt
@@ -0,0 +1,20 @@
+package io.github.ziginsider.kotlinthreaddemo.delegate.property
+
+/**
+ * Created by zigin on 04.02.2018.
+ */
+
+class PersonSecond(val name: String,
+                   age: Int,
+                   salary: Int): PropertyChangeAware() {
+
+    val _age = ObservableProperty("age", age, changeSupport)
+    var age: Int
+        get() = _age.getValue()
+        set(value) { _age.setValue(value)}
+
+    val _salary = ObservableProperty("salary", salary, changeSupport)
+    var salary: Int
+        get() = _salary.getValue()
+        set(value) { _salary.setValue(value)}
+}
